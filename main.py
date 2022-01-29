30db268dd71879b7ff55c7e7b476a99d6e60980c
Yi Fan Li <yifanli123@gmail.com>
2022-01-29 11:03:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def main_warpper():
+    #Attributes are a thing
+    print(f"this is the start of our python project. This function's name is {main_warpper.__name__}")
+
+    # Code Here
+
+    print("this is the end of our python project. ")
+
+
+if __name__ == "__main__":
+    main_warpper()
